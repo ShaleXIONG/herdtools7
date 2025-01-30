f595b64b2b760b0323fff93344e0602d3274f75d
Shale Xiong <xiongshale@gmail.com>
2025-01-30 13:18:26 +0000

    Draft for generating all the fault check information.
    - Fix the fault check in cycle. In read it checks against a potential
      old value.
    - Generate the fault check condition in the post-condition.

--- gen/FaultSet.ml
+++ gen/FaultSet.ml
@@ -1,12 +1,15 @@
 module FaultSet = MySet.Make(
     struct
-        type t = string option * string
-        let compare (lhs_opt_label, lhs_var) (rhs_opt_label, rhs_var) = 
-            let opt_label = 
-                Option.compare String.compare lhs_opt_label rhs_opt_label in
-            if opt_label = 0 then
-                String.compare lhs_var rhs_var
-            else opt_label
+        type t = string option * bool option * string
+        let compare (lhs_label, lhs_bool, lhs_var) (rhs_label, rhs_bool, rhs_var) = 
+            let label = 
+                Option.compare String.compare lhs_label rhs_label in
+            let boolean =
+                Option.compare Bool.compare lhs_bool rhs_bool in
+            match label, boolean with
+            | 0, 0 -> String.compare lhs_var rhs_var
+            | 0, boolean -> boolean
+            | label, _ -> label
     end
     )
 
